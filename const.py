359e36c388ba656328405fe2b957d84c6cc6e081
BlackLik <alssval@gmail.com>
2021-03-14 01:38:32 +0300

    v 1.0

---
+++ const.py
@@ -0,0 +1,4 @@
+URL = 'https://www.avito.ru/moskva?q=%D0%BA%D0%BE%D0%BC%D0%BF%D1%8C%D1%82%D0%B5%D1%80'
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+           'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
+HOST = 'https://www.avito.ru/'
--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+import requests
+import const
+from bs4 import BeautifulSoup
 
+
+def get_html(url, params=None):
+    r = requests.get(url=url, params=params, headers=const.HEADERS,)
+    return r
+
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('div', class_='iva-item-body-NPl6W')
+    objects = []
+    for item in items:
+        if item is not None:
+        objects.append({
+            'title': item.find('h3', class_='title-root-395AQ iva-item-title-1Rmmj title-list-1IIB_ title-root_maxHeight-3obWc text-text-1PdBw text-size-s-1PUdo text-bold-3R9dt').get_text(strip=True),
+            'price': ''.join(item.find('span', class_='price-text-1HrJ_ text-text-1PdBw text-size-s-1PUdo').get_text().split('\xa0₽')[0].split(' ')),
+            'link': const.HOST + item.find('a', class_='link-link-39EVK link-design-default-2sPEv title-root-395AQ iva-item-title-1Rmmj title-list-1IIB_ title-root_maxHeight-3obWc').get('href')
+        })
+    for object in objects:
+        print(object)
+
+
+def main():
+    html = get_html(const.URL)
+    if html.status_code == 200:
+        get_content(html.text)
+    else:
+        print('Error: html status not 200')
 
 
 if __name__ == '__main__':
-    pass
+    main()
