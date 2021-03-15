6b15c2e4f07c9541c45f4441f2b410a58faedf9b
BlackLik <alssval@gmail.com>
2021-03-15 23:38:42 +0300

    Finish v 1.0

--- const.py
+++ const.py
@@ -1,4 +1,5 @@
-URL = 'https://www.avito.ru/moskva?q=%D0%BA%D0%BE%D0%BC%D0%BF%D1%8C%D1%82%D0%B5%D1%80'
-HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+URL = 'https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie-ASgBAgICAUTGB~pm?bt=1&q=nvidia+geforce+gtx+1060'
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.54',
            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
 HOST = 'https://www.avito.ru/'
+FILE = 'objects.csv'
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import requests
 import const
 from bs4 import BeautifulSoup
+import csv
 
 
 def get_html(url, params=None):
-    r = requests.get(url=url, params=params, headers=const.HEADERS,)
+    r = requests.get(url=url, params=params, headers=const.HEADERS)
     return r
+
+
+def get_pages_count(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    pagination = soup.find_all('span', class_='pagination-item-1WyVp')
+    if pagination:
+        return int(pagination[-2].get_text())
+    else:
+        return 1
 
 
 def get_content(html):
@@ -13,20 +23,46 @@ def get_content(html):
     items = soup.find_all('div', class_='iva-item-body-NPl6W')
     objects = []
     for item in items:
-        if item is not None:
-        objects.append({
-            'title': item.find('h3', class_='title-root-395AQ iva-item-title-1Rmmj title-list-1IIB_ title-root_maxHeight-3obWc text-text-1PdBw text-size-s-1PUdo text-bold-3R9dt').get_text(strip=True),
-            'price': ''.join(item.find('span', class_='price-text-1HrJ_ text-text-1PdBw text-size-s-1PUdo').get_text().split('\xa0₽')[0].split(' ')),
-            'link': const.HOST + item.find('a', class_='link-link-39EVK link-design-default-2sPEv title-root-395AQ iva-item-title-1Rmmj title-list-1IIB_ title-root_maxHeight-3obWc').get('href')
-        })
-    for object in objects:
-        print(object)
+        if item.text != {}:
+            price = ''.join(item.find('span', class_='price-text-1HrJ_').get_text().split('\xa0₽')[0].split(' '))
+            if price == 'Ценанеуказана':
+                price = 'Цена не указана'
+            elif price == 'Бесплатно':
+                price = 0
+            else:
+                price = int(price)
+            objects.append({
+                'title': item.find('h3', class_='title-root-395AQ').get_text(),
+                'price': price,
+                'link': const.HOST + item.find('a', class_='link-link-39EVK').get('href')
+            })
+        else:
+            continue
+    return objects
+
+
+def save_data(items, path):
+    with open(path, 'w', newline='') as file:
+        writter = csv.writer(file, delimiter=';')
+        writter.writerow(['Загаловок', 'Цена', 'Ссылка'])
+        for item in items:
+            writter.writerow([item['title'], item['price'], item['link']])
 
 
 def main():
     html = get_html(const.URL)
     if html.status_code == 200:
-        get_content(html.text)
+        objects = []
+        pages_count = get_pages_count(html.text)
+        if pages_count >= 3:
+            pages_count = 3
+        for page in range(1, pages_count + 1):
+            print('Сейчас страница {} из {}'.format(page, pages_count))
+            html = get_html(const.URL, params={'p': page})
+            objects.extend(get_content(html.text))
+        save_data(objects, const.FILE)
+        print("Оконченно")
+        print("Всего объектов: ", len(objects))
     else:
         print('Error: html status not 200')
 
